2a7efb25af9c94b3e6972a0d3da3ad66e396197e
Hanage999 <hanage999@crazynewworld.net>
2019-08-17 06:27:50 +0900

    Change CSP to allow tesseract.js

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -22,7 +22,7 @@ Rails.application.config.content_security_policy do |p|
   p.base_uri        :none
   p.default_src     :none
   p.frame_ancestors :none
-  p.font_src        :self, assets_host
+  p.font_src        :self, assets_host, :data
   p.img_src         :self, :https, :data, :blob, assets_host
   p.style_src       :self, assets_host
   p.media_src       :self, :https, :data, assets_host
@@ -38,11 +38,13 @@ Rails.application.config.content_security_policy do |p|
     p.script_src  :self, :unsafe_inline, :unsafe_eval, assets_host
     p.child_src   :self, :blob, assets_host
     p.worker_src  :self, :blob, assets_host
+    p.child_src   :self, :blob, assets_host
   else
     p.connect_src :self, :data, :blob, assets_host, media_host, Rails.configuration.x.streaming_api_base_url
     p.script_src  :self, assets_host, "'wasm-unsafe-eval'"
     p.child_src   :self, :blob, assets_host
     p.worker_src  :self, :blob, assets_host
+    p.child_src   :self, :blob, assets_host
   end
 end
 
